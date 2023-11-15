60f0a507c910ecd1889c784b36f3c4977eb24020
Pavel Dolganov mSt <fire_ra@bk.ru>
2023-11-15 18:21:47 +0300

    added userStorage

---
+++ app/src/main/java/com/example/cleanarckoval220123/data/storage/models/User.kt
@@ -0,0 +1,7 @@
+package com.example.cleanarckoval220123.data.storage.models
+
+class User(
+    val firstName: String,
+    val lastName: String
+) {
+}
---
+++ app/src/main/java/com/example/cleanarckoval220123/data/storage/sharedprefs/SharedPrefUserStorage.kt
@@ -0,0 +1,29 @@
+package com.example.cleanarckoval220123.data.storage.sharedprefs
+
+import android.content.Context
+import com.example.cleanarckoval220123.data.storage.UserStorage
+import com.example.cleanarckoval220123.data.storage.models.User
+
+private const val SHARED_PREFS_NAME = "shared_preff_name"
+private const val KEY_FIRST_NAME = "firstName"
+private const val KEY_LAST_NAME = "lastName"
+private const val DEFAULT_FIRST_NAME = "Default first name"
+private const val DEFAULT_LAST_NAME = "Default last name"
+
+class SharedPrefUserStorage(context: Context): UserStorage {
+
+    private val sharedPreferences = context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+
+    override fun save(user: User): Boolean {
+        sharedPreferences.edit().putString(KEY_FIRST_NAME, user.firstName).apply()
+        sharedPreferences.edit().putString(KEY_LAST_NAME, user.lastName).apply()
+        return true
+    }
+
+    override fun get(): User {
+        val firstName = sharedPreferences.getString(KEY_FIRST_NAME, DEFAULT_FIRST_NAME) ?: DEFAULT_FIRST_NAME
+        val lastName = sharedPreferences.getString(KEY_LAST_NAME, DEFAULT_LAST_NAME) ?: DEFAULT_LAST_NAME
+
+        return User(firstName, lastName)
+    }
+}
